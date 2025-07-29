f7d44c3619748fd3d61a783ab14e81e8aa94505f
MieJM98 <142306393+MieJM98@users.noreply.github.com>
2025-07-29 17:20:12 +0300

    s

---
+++ scr/rch2.js
@@ -0,0 +1,10 @@
+// Liste des termes GitHub à surveiller
+const githubTerms = [
+  "3001 / 92%", // Très actif, souvent posté
+  "1005 / 100%", // Parfait, recruteur fiable
+  "58 / 95%", // Bon score, pas toujours actif
+  "445 / 96%", // Stable
+  "174 / 90%", // Moyen
+  "163 / 89%", // Limite inférieure
+  "247 / 97%" // Très fiable
+];
